eee6314e5ab94d8250b020a13a3e8d75518e98a8
David Mohundro <drmohundro@gmail.com>
2010-11-08 22:12:25 -0600

    Handling more words that should be treated as one word

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -1,12 +1,26 @@
 class String
   def camelcase_to_dashes
-    self.to_s.gsub(/::/, '/').
+    val = to_s.gsub(/::/, '/').
       gsub(/([A-Z]+)([A-Z][a-z])/,'\1-\2').
       gsub(/([a-z\d])([A-Z])/,'\1-\2').
-      downcase.
-      gsub('power-shell', 'powershell').
-      gsub('win-dbg', 'windbg').
-      gsub('event-setters', 'eventsetters').
-      gsub('manual-reset-event', 'manualresetevent')
+      downcase
+
+    singular_compound_words().each do |word|
+      val.gsub! word, word.gsub('-', '')
+    end
+
+    val.gsub! 'net20', 'net-20'
+    val.gsub! 'bindinglist-databinding-the-way-you-would-expect-in-net-20', 'bindinglist---data-binding-the-way-you-would-expect-in-net-20'
+
+    val
+  end
+
+  private
+
+  def singular_compound_words
+    ['power-shell', 'win-dbg', 'event-setters', 'manual-reset-event', 'tools-install-community-controls',
+     'file-helpers', 'ftp-web-request-ftp-web-response', 'win-snap', 'flow-layout-panel', 'outer-html',
+     'data-binding', 'managed-spy', 'feed-burner', 'background-worker', 'i-support-initialize', 'ms-build',
+     'code-plex', 'x-path', 'binding-list', 'design-mode', 'browsable-attribute', 'one-care']
   end
 end
