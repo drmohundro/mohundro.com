2825552936946f83930e54c8d632cc8bc8b8987c
David Mohundro <drmohundro@gmail.com>
2010-12-13 15:40:23 -0600

    Added footer, some reformatting.

--- lib/site.rb
+++ lib/site.rb
@@ -1,14 +1,14 @@
 class Site
   def call(env)
     path = env['PATH_INFO'].gsub! '/', ''
-    http_return, content = render_path(path)
+    status, content = get_status_and_content(path)
 
-    [http_return, {"Content-Type" => "text/html"}, [content]]
+    [status, {"Content-Type" => "text/html"}, [content]]
   end
 
   private
 
-  def render_path(path)
+  def get_status_and_content(path)
     path = 'root' if path == ''
     http_return = 200
 
@@ -19,11 +19,19 @@ class Site
       full_path = qualify_path '404'
     end
 
-    return http_return, ERB.new(File.read(full_path)).result(binding)
+    return http_return, render(full_path)
   end
 
   def header
-    ERB.new(File.read(qualify_path '_header')).result(binding)
+    render(qualify_path '_header')
+  end
+
+  def footer
+    render(qualify_path '_footer')
+  end
+
+  def render(path)
+    ERB.new(File.read(path)).result(binding)
   end
 
   def qualify_path(path)
