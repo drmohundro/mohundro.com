4fab8dc64d86bda3af208a057930786104fdafe7
David Mohundro <drmohundro@gmail.com>
2011-01-12 21:38:58 -0600

    Obscure the email address

--- lib/site.rb
+++ lib/site.rb
@@ -10,6 +10,17 @@ class Site
     render(qualify_path '404')
   end
 
+  def obscure_email(email)
+    return nil if email.nil? #Don't bother if the parameter is nil.
+    lower = ('a'..'z').to_a
+    upper = ('A'..'Z').to_a
+    email.split('').map { |char|
+      output = lower.index(char) + 97 if lower.include?(char)
+      output = upper.index(char) + 65 if upper.include?(char)
+      output ? "&##{output};" : (char == '@' ? '&#0064;' : char)
+    }.join
+  end
+
   private
 
   def get_status_and_content(path)
